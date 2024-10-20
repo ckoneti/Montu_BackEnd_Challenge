767ae3e9c7475bf1233c7691c1f8661a85bfee30
CK <chitrakoneti@gmail.com>
2024-10-20 22:59:20 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,14 @@ import {address} from "../types";
 // function that takes partial address as input and returns specific address details
 export async function getAutoCompleteDetails(address: string): Promise<address[]> {
     const apiKey: string = process.env.TOMTOM_API_KEY || '';
+    console.log(apiKey,'apikey')
     if (!address) {
         throw new Error('Address is required')
     }
     // func call to get the address details
     return await getPlaceAutocomplete(apiKey, address)
 }
-
-getAutoCompleteDetails('Main Street').then(address => {
+//Uncomment to run the func
+/*getAutoCompleteDetails('Main Street').then(address => {
     console.log(address)
-})
-
-
-
-
-
-
+})*/
